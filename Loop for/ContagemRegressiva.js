ea24048f16e1396f469a42e64beb4a26f115df4c
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-05-15 19:09:53 -0300

    contagem de N até 0

---
+++ Loop for/ContagemRegressiva.js
@@ -0,0 +1,13 @@
+// Declare um número N e imprima no console a contagem de N até 0.
+
+for (let n = 10; n >= 0; n --){
+    console.log(n)
+}
+
+/*
+for (let i = 0; i < 9; i++) 
+
+for (inicialização; condição (enquanto for verdadeira); iteração){ 
+
+}
+*/
